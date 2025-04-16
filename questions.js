56dec365e1acb79d9689f28ffa61afb2e0dade98
BurhanHelmy-tech <buhanhelmy44@gmail.com>
2025-04-16 08:55:16 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Jumlah sudut pedalaman segitiga",
+        answer: "B. 180",
+        options: [
+            "A. 190",
+            "B. 180",
+            "C. 170",
+            "D. 150"
+        ]
+    },
+    {
+        numb: 2,
+        question: "2a + 2a + 2a = ",
+        answer: "A. 6a",
+        options: [
+            "A. 6a",
+            "B. 6a⁴",
+            "C. 8a",
+            "D. 8a⁴"
+        ]
+    },
+    {
+        numb: 3,
+        question: "2a x 2a x 2a x 2a = ",
+        answer: "D. 16a⁴",
+        options: [
+            "A. 8a⁴",
+            "B. 8a",
+            "C. 16a",
+            "D. 16a⁴"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Faktorkan x² + 5x + 6",
+        answer: "D. (x + 3)(x + 2)",
+        options: [
+            "A. (x + 5)(x+1)",
+            "B. (x + 4)(x + 2)",
+            "C. (x + 6)(x + 1)",
+            "D. (x + 3)(x + 2)"
+        ]
+    },
+    {
+        numb: 5,
+        question: "2a + 2 = 10, Cari a.",
+        answer: "C. 4",
+        options: [
+            "A. 12",
+            "B. 8",
+            "C. 4",
+            "D. 6"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Kecerunan garis yang melalui titik (2, 2) dan titik (6,6)",
+        answer: "A. 1",
+        options: [
+        "A. 1",
+        "B. 2",
+        "C. 3",
+        "D. 4"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Kecerunan garis yang melalui titik(2a, 2a) dan (6a, 6a)",
+        answer: "D. 1",
+        options: [
+        "A. 4",
+        "B. 3",
+        "C. 2",
+        "D. 1"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Diberi segitiga ABC. AB = 6 cm, BC = 8 cm. Sudut ABC = 90°.Cari perimeter segitiga ABC.",
+        answer: "C. 24",
+        options: [
+        "A. 26",
+        "B. 18",
+        "C. 24",
+        "D. 22"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Bundarkan 3854 kepada satu angka beerti",
+        answer: "C. 3000",
+        options: [
+        "A. 3",
+        "B. 4",
+        "C. 3000",
+        "D. 4000"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Nyatakan 1750 dalam bentuk piawai.",
+        answer: "A. 1.75 x 10³",
+        options: [
+        "A. 1.75 x 10³",
+        "B. 1.75 x 10²",
+        "C. 17.5 x 10³",
+        "D. 17.5 x 10"
+        ]
+    },
+];
